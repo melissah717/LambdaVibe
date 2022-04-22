e7c9de8ce20323cc59d570cd3c302b95c98aac60
Ze Lei <zlei1@mail.sfsu.edu>
2022-04-22 14:49:59 -0700

    add amplitude to visualizer

--- client/src/visualizers/Frogger-Software.tsx
+++ client/src/visualizers/Frogger-Software.tsx
@@ -18,15 +18,27 @@ export const FroggerSoftwareVisualizer = new Visualizer(
         p5.stroke(255, 255, 255, 255);
         p5.noFill();
 
+        // const fftAnalyzer = new Tone.Analyser(analyzer);
+        // fftAnalyzer.type = "fft";
         const levels = analyzer.getValue();
+        // const values = fftAnalyzer.getValue();
 
         p5.beginShape();
         for (let i = 0; i < levels.length; i++) {
-            const amplitude = levels[i] as number;
-            let levelHeight = p5.map(amplitude, 0, 0.4, 0, height);
-            const x = p5.map(i, 0, levels.length - 1, 0, width);
-            p5.fill(100, 250, 100);
+            const amplitude = levels[i] as number * 400;
+            // const frequency = values[i] as number;
+            let levelHeight = p5.map(amplitude/200, 0, 0.4, 0, height/2);
+            const x = p5.map(i, 0, levels.length, 0, width);
+            p5.fill(100, 100, 250);
             p5.rect(0, height, x, -levelHeight);
+
+            // p5.fill(frequency, frequency, 100);
+            // const x = p5.map(i, 0, levels.length / 16, 0, width);
+            // let h = p5.map(frequency/100, -1, 1, 0, height);
+            // p5.rect(x, height, levels.length / 4, -h/2);
+
+
+            p5.ellipse(width / 2, height / 4, 10 + amplitude, 10 + amplitude);
         }
         p5.endShape();
     },

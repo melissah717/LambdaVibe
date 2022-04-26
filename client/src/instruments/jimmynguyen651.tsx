5bc431f7da5afed17641fe0b0d47e7e4167d9f01
jimmynguyen651 <56140887+jimmynguyen651@users.noreply.github.com>
2022-04-26 12:22:16 -0500

    changed instrument idea + sounds

--- client/src/instruments/jimmynguyen651.tsx
+++ client/src/instruments/jimmynguyen651.tsx
@@ -12,15 +12,28 @@ import { Instrument, InstrumentProps } from '../Instruments';
  ** ------------------------------------------------------------------------ */
 
 
- const sampler = new Tone.Sampler({
-	urls: {
-		// letter determines actual note 
-    E1: "https://cdn.kapwing.com/final_626576f65407960659cd4aaa_17861.mp3"
-	},
+//  const sampler = new Tone.Sampler({
+// 	urls: {
+// 		// letter determines actual note 
+//     E1: "https://cdn.kapwing.com/final_626576f65407960659cd4aaa_17861.mp3"
+
+// 	},
+ 
+// }).toDestination();
+
+const sampler = new Tone.Sampler({
+  urls: {
+    C1: "https://cdn.kapwing.com/final_62682213960a8700766ca7de_141750.mp3", // first
+    C2: "https://cdn.kapwing.com/final_626822a54a94a30114cae76e_537685.mp3", // second
+    C3: "https://cdn.kapwing.com/final_62682611547067008ddbca49_355155.mp3", //third
+    C4: "https://cdn.kapwing.com/final_6268298dd540f9006733fd80_779826.mp3", // fourth
+  }
+
 }).toDestination();
 
 
-interface GuitarNotesProps {
+
+interface VaporwaveNotesProps {
   note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
   duration?: string;
   synth?: Tone.Sampler; // Contains library code for making sound
@@ -29,73 +42,54 @@ interface GuitarNotesProps {
   index: number; // octave + index together give a location for the piano key
 }
 
-export function GuitarNotes({
+export function VaporwaveNotes({
   note,
   synth,
   minor,
   index,
-}: GuitarNotesProps): JSX.Element {
-  /**
-   * This React component corresponds to either a major or minor key in the piano.
-   * See `GuitarKeyWithoutJSX` for the React component without JSX.
-   */
+}: VaporwaveNotesProps): JSX.Element {
+ 
   return (
     // Observations:
     // 1. The JSX refers to the HTML-looking syntax within TypeScript.
     // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
     // 3. The curly braces `{` and `}` should remind you of string interpolation.
+    <div  className={classNames('ba absolute', {
+      
+      'black bg-white h4': !minor, 
+    })}
+    
+    style = {{
+      width: '200px',
+      marginRight: '200px'
+    }}>
+    "container but not clickable box here"
     <div
       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
       onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
       className={classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor, // minor keys are black
+     
         'black bg-white h4': !minor, // major keys are white
       })}
       style={{
         // CSS
         top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
+        left: `${index * 4}rem`,
+        zIndex: minor ? 2 : 0,
         width: minor ? '1.5rem' : '2rem',
         marginLeft: minor ? '0.25rem' : 0,
-      }}
-    ></div>
+        height: '20px'
+      }}>
+    
+
+    </div>
+    </div>
   );
 }
 
-// eslint-disable-next-line
-function GuitarKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: GuitarNotesProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `GuitarNotes` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      },
-    },
-    [],
-  );
-}
 
-function GuitarType({ title, onClick, active }: any): JSX.Element {
+
+function VaporwaveType({ title, onClick, active }: any): JSX.Element {
   return (
     <div
       onClick={onClick}
@@ -111,7 +105,7 @@ function GuitarType({ title, onClick, active }: any): JSX.Element {
   );
 }
 
-function Guitar({ synth, setSynth }: InstrumentProps): JSX.Element {
+function Vaporwave({ synth, setSynth }: InstrumentProps): JSX.Element {
   const keys = List([
     { note: 'C', idx: 0},
     { note: 'Db', idx: 0.5 },
@@ -158,7 +152,7 @@ function Guitar({ synth, setSynth }: InstrumentProps): JSX.Element {
             const isMinor = key.note.indexOf('b') !== -1;
             const note = `${key.note}${octave}`;
             return (
-              <GuitarNotes
+              <VaporwaveNotes
                 key={note} //react key
                 note={note}
                 synth={sampler} // PUT SAMPLE HERE?
@@ -172,7 +166,7 @@ function Guitar({ synth, setSynth }: InstrumentProps): JSX.Element {
       </div>
       <div className={'pl4 pt4 flex'}>
         {oscillators.map(o => (
-          <GuitarType
+          <VaporwaveType
             key={o}
             title={o}
             onClick={() => setOscillator(o)}
@@ -184,4 +178,4 @@ function Guitar({ synth, setSynth }: InstrumentProps): JSX.Element {
   );
 }
 
-export const GuitarInstrument = new Instrument('Guitar', Guitar);
+export const VaporwaveInstrument = new Instrument('Vaporwave', Vaporwave);

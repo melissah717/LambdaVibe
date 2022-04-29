b3a31a3f0790be19607a93b50c3bf768a2f9f067
Melissa Ho <mho10@mail.sfsu.edu>
2022-04-28 19:22:33 -0700

    Added Concertino in C

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -5,4 +5,5 @@ CREATE TABLE songs (
 );
 
 INSERT INTO songs (id, song_title, notes) 
-VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4'),
+(2, 'Concertino in C by Chaminade', 'D3 E3 Gb3 A3 Gb3 Gb3 E3 D3 E3 D3 D3 Db3 B2 A2 B2 Db3 D3 Gb3 G3 B3 A3 Gb3 E3 D3 B2 E3 D3');

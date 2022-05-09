9f92467018bfff5012b1077179e71af824980ce5
Sami <67216252+saxgetty@users.noreply.github.com>
2022-05-08 21:45:41 -0700

    Added Epic Sax song to the database

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -13,4 +13,5 @@ VALUES (1, 'Ode to Joy (Dubstep Remix)', 'DJ Mobley', 'E4 E4 F4 G4 G4 F4 E4 D4 C
 (5, 'To Zanarkand', 'Nobuo Uematsu', 'E3 B2 E3 Gb3 G3 Gb3 E3 D3 E3 D3 B2 E3 B2 E3 Gb3 G3 Gb3 G3 A3 G3 A3 B3'),
 (6, 'Sing to the Moon', 'Lauren Mvula', 'D3 C3 D3 F3 F3 F3 F3 D3 C3 C3 Bb2 D3 D3 D3 D3 C3 Bb2 Bb2 G2 D3 C3 D3 D3 D3 G3 G3 G3 G3 F3 D3 D3 C3 D3 D3 C3 Bb2 Bb2 G2'),
 (7, 'In The Air Tonight', 'Phil Collins', 'E1 A2 E2 A3 E3 A4 E1 A2 E2 A3 E3 A4'), -- Alternative After Drum Fill: E1 A2 E2 A3 E3 A4
-(8, 'Vampires', 'The Midnight', 'Bb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1')
+(8, 'Vampires', 'The Midnight', 'Bb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1 Db1 Ab3 Gb1'),
+(9, 'Epic Sax', 'Sergey Stepanov', 'B1 B1 B1 B1 B1 A1 B1 B1 B1 B1 B1 A1 B1 B1 D1 B1 A1 G1 E1 E1 F1 G1 E1')
